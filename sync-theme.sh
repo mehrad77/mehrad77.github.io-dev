fe4a89dcb6a70936685d053a0d81a35d83fc4688
Mehrad Rousta <mehrad77@gmail.com>
2018-04-06 00:55:32 +0430

    Adding Changes to sync-theme.sh script (about git)

--- sync-theme.sh
+++ sync-theme.sh
@@ -7,21 +7,28 @@ cd en/themes/cactus/
 git add .
 git commit -m "`LC_TIME=en_US date`" 
 git push -u origin master
+git stash pop
 cd ../../
 
 echo "$(tput setaf 1) de pulling $(tput sgr 0)"
 cd ../de/themes/cactus/
+git stash
 git pull --force
+git stash pop
 cd ../../
 
 echo "$(tput setaf 1) fa pulling $(tput sgr 0)"
 cd ../fa/themes/cactus/
+git stash
 git pull --force
+git stash pop
 cd ../../
 
 echo "$(tput setaf 1) per pulling $(tput sgr 0)"
 cd ../per/themes/cactus/
+git stash
 git pull --force
+git stash pop
 cd ../../
 
 echo "$(tput setaf 2) ==[All synced!]== $(tput sgr 0)"
